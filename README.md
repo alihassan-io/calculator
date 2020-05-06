c811c6f876ff50e8d86716c130b38408d71a2e1b
AliHassan172 <15081598-172@uog.edu.pk>
2020-05-06 17:52:00 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Calculator
+
+This is a simple basic calculator. This is a web based application. This basic calculator can be used for addition, subtraction, division and multiplication.
+
+## Built With
+
+- [HTML5](https://www.w3schools.com/html/default.asp) - The Use of Semantic HTML
+- [CSS3 Grid](https://www.w3schools.com/css/css3_flexbox.asp) - The CSS3 Web Layout Model
+- [Vanilla Js](http://vanilla-js.com/) - Fast, lightweight, cross-platform framework for building incredible, powerful JavaScript applications.
+
+## Authors
+
+- **Ali Hassan** - _other work_ - [GitHub](https://github.com/AliHassanAlraee "Ali Hassan Alraee")
+
+## Acknowledgments
+
+- Design inspired by [iOS-11 Calculator](https://apps.apple.com/gb/app/the-calculator/id398129933)
---
+++ app.js
@@ -0,0 +1,95 @@
+// UI Variables
+let inputField = document.querySelector("#input");
+const buttons = document.querySelector(".buttons");
+
+// Load All Events
+loadAllEventListeners();
+
+// Load All Event Listeners
+function loadAllEventListeners() {
+    // Buttons click event (Event Delegation)
+    buttons.addEventListener("click", calculate);
+}
+
+// Calculate
+function calculate(e) {
+    let value = inputField.value;
+
+    // If button is pressed
+    if (e.target.classList.contains("btn")) {
+        // Action Category of button
+        if (e.target.classList.contains("btn-action")) {
+            if (e.target.value === "c") {
+                value = "";
+            }
+            if (e.target.value === "+/-") {
+                if (value != "") {
+                    value = Number(value);
+                    value = -(value);
+                    value = String(value);
+                }
+            }
+            if (e.target.value === "%") {
+                console.log("% Pressed");
+            }
+        }
+
+        // Action Category of button
+        else if (e.target.classList.contains("btn-operation")) {
+            if (e.target.value === "/") {
+                if (value != "") {
+                    value += "/";
+                }
+            }
+            if (e.target.value === "x") {
+                if (value != "") {
+                    value += "x";
+                }
+            }
+            if (e.target.value === "-") {
+                if (value != "") {
+                    value += "-";
+                }
+            }
+            if (e.target.value === "+") {
+                if (value != "") {
+                    value += "+";
+                }
+            }
+            if (e.target.value === "=") {
+                console.log("= Pressed");
+            }
+        }
+
+        // Number Category of button
+        else if (e.target.classList.contains("btn-number")) {
+            if (e.target.value === ".") {
+                if (value.indexOf(".") === -1) {
+                    value += ".";
+                }
+            } else if (e.target.value === "0") {
+                value += "0";
+            } else if (e.target.value === "1") {
+                value += "1";
+            } else if (e.target.value === "2") {
+                value += "2";
+            } else if (e.target.value === "3") {
+                value += "3";
+            } else if (e.target.value === "4") {
+                value += "4";
+            } else if (e.target.value === "5") {
+                value += "5";
+            } else if (e.target.value === "6") {
+                value += "6";
+            } else if (e.target.value === "7") {
+                value += "7";
+            } else if (e.target.value === "8") {
+                value += "8";
+            } else if (e.target.value === "9") {
+                value += "9";
+            }
+
+        }
+    }
+    inputField.value = value;
+}
